fed16ce6217ec2bddcf875984fcc5891a3b7125f
Frontend JavaScript <frontendjavascript@C3-10.local>
2023-05-18 20:41:59 +0300

    curs

---
+++ mycrud/backend/types/User.ts
@@ -0,0 +1,20 @@
+ interface BasicUser {
+    id: number,
+  }
+   interface User extends BasicUser {
+    nume: string,
+    prenume: string,
+    datanastere:Date, //string
+    email:string,
+    telefon: string,
+    poza?:string,
+    dataadaugare?:Date,
+    actiune?:string
+  }
+   interface UserWithDetails extends BasicUser, User{
+    userId: number,
+    user: User,
+    
+  }
+
+  export {BasicUser ,User ,UserWithDetails}
